6885410251999d269bb2078c1dd373472b963442
RonaldJoyTengco <RonaldJoyTengco@yahoo.com>
2025-05-03 21:58:52 +0800

    Created a login form

--- index.php
+++ index.php
@@ -3,3 +3,21 @@
 echo "hello this is the jenkins tutorial";
 echo "<br/>";
 echo "This is the sub slogan";
+?>
+<html>
+
+<head>
+    <title>Login Page</title>
+</head>
+
+<body>
+    <form action="" method="POST">
+        <label>Username:</label>
+        <input type="text" value="" name="username">
+        <label>Password</label>
+        <input type="password" value="" name="password">
+        <input type="submit" value="Login" name="loginButton">
+    </form>
+</body>
+
+</html>
